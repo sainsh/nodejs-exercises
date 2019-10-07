0451069a533be17bc6f44b31eb3a424855a4736e
sainsh <kfsainsh@gmail.com>
2019-10-07 15:52:44 +0200

    followed witt's example changed it to work with json objects

--- node1.js
+++ node1.js
@@ -1,24 +1,27 @@
 const http = require('http');
 const fs = require('fs')
 
-var server = http.createServer(function(req,res){
-        fs.readFile('./counter.json',  (err, json) => {
-            if (err) {
-                console.log(err)
-                return
-                
-            }
-                var file = JSON.parse(json);
-            
-            res.writeHead(200, {'Content-Type' : 'application/json'});
-            res.write(JSON.stringify(file));
-        
-            fs.writeFile('./counter.json', (err, json )=>{
+var server = http.createServer(function (req, res) {
+    fs.readFile('counter.json', (err, json) => {
 
-                
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        if (req.url != "/favicon.ico") {
+
+            var count = JSON.parse(json);
+
+            count.count  += 1
+
+            fs.writeFile('counter.json', JSON.stringify(count), function (err) {
+                console.log('saved' + req.rul)
             })
             
-            res.end();
-        
-});
+            res.end(JSON.stringify(count) + " ");
+        }
+
+
+
+
+
+    });
 }).listen(8080);
