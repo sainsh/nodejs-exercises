a8d546f63cb4ea192b6653e080f0d5f5f99e7aef
sainsh <kfsainsh@gmail.com>
2019-10-09 09:38:31 +0200

    tried writing to res multiple times this did not work, so returned to previous

--- node1.js
+++ node1.js
@@ -1,27 +1,30 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const date = require('./dato');
 
 var server = http.createServer(function (req, res) {
-    fs.readFile('counter.json', (err, json) => {
+    
+    fs.readFile('index.html', (error, html) => {
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('counter.json', (err, json) => {
 
-        if (req.url != "/favicon.ico") {
+            if (req.url != "/favicon.ico") {
 
-            var count = JSON.parse(json);
+                var count = JSON.parse(json);
 
-            count.count  += 1
+                count.count += 1
 
-            fs.writeFile('counter.json', JSON.stringify(count), function (err) {
-                console.log('saved' + req.rul)
-            })
-            
-            res.end(JSON.stringify(count) + " ");
-        }
+                fs.writeFile('counter.json', JSON.stringify(count), function (err) {
+                    console.log('saved' + req.rul)
+                })
+            }
 
+            res.writeHead(200, { 'Content-Type': 'text/html' });
 
+            res.write(html + " " + JSON.stringify(count) + " " + date.myDateTime())
 
+            res.end();
 
-
+        });
     });
 }).listen(8080);
